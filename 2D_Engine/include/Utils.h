91c216f0fe92e233cb5f8795bc121bfd3edbc368
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-08 18:04:48 +0530

    Create Utils.h

---
+++ 2D_Engine/include/Utils.h
@@ -0,0 +1,16 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <SFML/Graphics.hpp>
+#include <cstdlib>
+#include <ctime>
+
+namespace Utils {
+    inline void initRandom() { std::srand(static_cast<unsigned int>(std::time(nullptr))); }
+    inline sf::Color randomColor() { return sf::Color(std::rand() % 255, std::rand() % 255, std::rand() % 255); }
+    inline float randomFloat(float min, float max) {
+        return min + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (max - min)));
+    }
+}
+
+#endif
