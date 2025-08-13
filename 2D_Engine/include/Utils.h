3a27a0c71ae4926042f83708af79c4a0e5109909
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 01:56:06 +0530

    Update Utils.h

--- 2D_Engine/include/Utils.h
+++ 2D_Engine/include/Utils.h
@@ -7,9 +7,9 @@
 
 namespace Utils {
     inline void initRandom() { std::srand(static_cast<unsigned int>(std::time(nullptr))); }
-    inline sf::Color randomColor() { return sf::Color(std::rand() % 255, std::rand() % 255, std::rand() % 255); }
+    inline sf::Color randomColor() { return sf::Color(std::rand() % 256, std::rand() % 256, std::rand() % 256); }
     inline float randomFloat(float min, float max) {
-        return min + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX / (max - min)));
+        return min + static_cast<float>(std::rand()) / (static_cast<float>(RAND_MAX) / (max - min));
     }
 }
 
