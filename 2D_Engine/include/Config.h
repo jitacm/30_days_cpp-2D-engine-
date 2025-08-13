a21633c8742dfe4a294777c2170cd7673694d8da
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 03:57:07 +0530

    Update Config.h

--- 2D_Engine/include/Config.h
+++ 2D_Engine/include/Config.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "Vector2.h"
 
-namespace Config
-{
-    // Global gravity vector applied to all rigid bodies
-    // Default: downward force similar to Earth's gravity
-    static inline Vector2 gravity = { 0.f, 500.f }; // px/s²
+namespace Config {
+    // pixels per second squared
+    static inline Vector2 gravity = {0.f, 500.f};
+
+    // global restitution if needed as fallback
+    static constexpr float DEFAULT_RESTITUTION = 0.2f;
 }
