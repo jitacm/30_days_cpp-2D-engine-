48dc66457dca4c60ea743ae0ea7e7a20890b9c9f
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 01:55:05 +0530

    Update RigidBody.cpp

--- 2D_Engine/src/RigidBody.cpp
+++ 2D_Engine/src/RigidBody.cpp
@@ -1,7 +1,11 @@
 #include "RigidBody.h"
+#include "Utils.h"
 
-RigidBody::RigidBody(std::unique_ptr<Shape> s, Vector2 pos, float m, bool stat)
-    : shape(std::move(s)), position(pos), mass(m), isStatic(stat), velocity(0,0) {}
+RigidBody::RigidBody(std::unique_ptr<Shape> s, Vector2 pos, float m, bool stat, sf::Color col)
+    : shape(std::move(s)), position(pos), mass(m), isStatic(stat), velocity(0,0), color(col) {
+    // ensure mass > 0 for non-static bodies
+    if (!isStatic && mass <= 0.0f) mass = 1.0f;
+}
 
 void RigidBody::applyImpulse(const Vector2& impulse) {
     if (!isStatic)
