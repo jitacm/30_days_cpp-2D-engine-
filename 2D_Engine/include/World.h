ab44ff69ef7298cb89ef5f79c7cfeb2c84026780
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 03:02:19 +0530

    Update World.h

--- 2D_Engine/include/World.h
+++ 2D_Engine/include/World.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <vector>
 #include "RigidBody.h"
+#include "Collision.h"
 
 class World
 {
 public:
     void addRigidBody(RigidBody* body);
     void update(float dt);
+
+    const std::vector<RigidBody*>& getBodies() const { return bodies; }
 
 private:
     std::vector<RigidBody*> bodies;
