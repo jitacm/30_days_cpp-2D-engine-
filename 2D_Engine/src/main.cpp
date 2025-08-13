8add1f09133998d538a981b1be25e84e99d6af5d
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 02:22:05 +0530

    Update main.cpp

--- 2D_Engine/src/main.cpp
+++ 2D_Engine/src/main.cpp
@@ -5,18 +5,18 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "2D Engine with Gravity & Forces");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "2D Engine with Gravity, Forces & Collision");
 
     World world;
 
-    // Circle object
+    // Circle with bounce
     CircleShape* circle = new CircleShape(20.f, {400.f, 100.f}, sf::Color::Green);
-    RigidBody* circleBody = new RigidBody(circle, 5.f);
+    RigidBody* circleBody = new RigidBody(circle, 5.f, 0.3f); // restitution = 0.3
     world.addRigidBody(circleBody);
 
-    // Rectangle object
+    // Rectangle without bounce
     RectangleShape* rect = new RectangleShape({50.f, 30.f}, {200.f, 50.f}, sf::Color::Blue);
-    RigidBody* rectBody = new RigidBody(rect, 10.f);
+    RigidBody* rectBody = new RigidBody(rect, 10.f, 0.0f); // no bounce
     world.addRigidBody(rectBody);
 
     sf::Clock clock;
@@ -29,15 +29,14 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            // Apply impulse when space is pressed
             if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
-                circleBody->applyImpulse({200.f, -300.f}); // Push right & up
+                circleBody->applyImpulse({200.f, -300.f}); // push up + right
         }
 
         float dt = clock.restart().asSeconds();
         world.update(dt);
 
-        window.clear();
+        window.clear(sf::Color::Black);
         circle->draw(window);
         rect->draw(window);
         window.display();
