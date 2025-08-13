6378ea64b46bf454c8863b867648d3ef3ee548a3
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 02:19:57 +0530

    Update main.cpp

--- 2D_Engine/src/main.cpp
+++ 2D_Engine/src/main.cpp
@@ -2,51 +2,44 @@
 #include "World.h"
 #include "CircleShape.h"
 #include "RectangleShape.h"
-#include "RigidBody.h"
-#include "Utils.h"
-#include "Config.h"
 
-int main() {
-    Utils::initRandom();
-
-    sf::RenderWindow window(sf::VideoMode(Config::WINDOW_WIDTH, Config::WINDOW_HEIGHT), "2D Engine - Multiple Shapes Demo");
-    window.setFramerateLimit(60);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "2D Engine with Gravity & Forces");
 
     World world;
 
-    // Create bodies using the new polymorphic rendering approach
-    RigidBody circle1(std::make_unique<CircleShape>(40.0f), Vector2(200, 300), 1.0f, false, Utils::randomColor());
-    RigidBody circle2(std::make_unique<CircleShape>(50.0f), Vector2(500, 300), 1.0f, false, Utils::randomColor());
-    circle2.velocity = Vector2(-120.0f, 0.0f);
+    // Circle object
+    CircleShape* circle = new CircleShape(20.f, {400.f, 100.f}, sf::Color::Green);
+    RigidBody* circleBody = new RigidBody(circle, 5.f);
+    world.addRigidBody(circleBody);
 
-    RigidBody rect1(std::make_unique<RectangleShape>(100.0f, 60.0f), Vector2(300, 500), 2.0f, false, Utils::randomColor());
-    RigidBody rect2(std::make_unique<RectangleShape>(120.0f, 80.0f), Vector2(600, 500), 1.5f, false, Utils::randomColor());
-    rect2.velocity = Vector2(-90.0f, 0.0f);
-
-    world.addBody(&circle1);
-    world.addBody(&circle2);
-    world.addBody(&rect1);
-    world.addBody(&rect2);
+    // Rectangle object
+    RectangleShape* rect = new RectangleShape({50.f, 30.f}, {200.f, 50.f}, sf::Color::Blue);
+    RigidBody* rectBody = new RigidBody(rect, 10.f);
+    world.addRigidBody(rectBody);
 
     sf::Clock clock;
-    while (window.isOpen()) {
+
+    while (window.isOpen())
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) window.close();
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            // Apply impulse when space is pressed
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+                circleBody->applyImpulse({200.f, -300.f}); // Push right & up
         }
 
         float dt = clock.restart().asSeconds();
-        world.step(dt);
+        world.update(dt);
 
-        window.clear(sf::Color::Black);
-
-        // Render all bodies (delegates to shape->render)
-        circle1.render(window);
-        circle2.render(window);
-        rect1.render(window);
-        rect2.render(window);
-
+        window.clear();
+        circle->draw(window);
+        rect->draw(window);
         window.display();
     }
-    return 0;
 }
