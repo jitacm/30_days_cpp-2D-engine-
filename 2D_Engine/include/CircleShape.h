968d120e81bcfe950f6df9866cd39fcb76c02d17
Ansh Dattaram Nikhare <145345576+AnshNikhare@users.noreply.github.com>
2025-08-14 01:53:46 +0530

    Update CircleShape.h

--- 2D_Engine/include/CircleShape.h
+++ 2D_Engine/include/CircleShape.h
@@ -1,8 +1,21 @@
 #pragma once
 #include "Shape.h"
+#include <SFML/Graphics.hpp>
 
 class CircleShape : public Shape {
 public:
     float radius;
     explicit CircleShape(float r) : Shape(ShapeType::Circle), radius(r) {}
+
+    void render(sf::RenderWindow& window, const Vector2& position, const sf::Color& color) const override {
+        sf::CircleShape s(radius);
+        s.setOrigin(radius, radius);
+        s.setPosition(position.x, position.y);
+        s.setFillColor(color);
+        window.draw(s);
+    }
+
+    float getBoundingRadius() const override {
+        return radius;
+    }
 };
